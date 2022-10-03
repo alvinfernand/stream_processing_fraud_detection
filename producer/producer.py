c0b7f563fbf73149055c5bc3b2f1f68c31a7c807
alvin <alvernandz@gmail.com>
2022-10-03 23:11:22 +0700

    Initail Commit

---
+++ producer/producer.py
@@ -0,0 +1,24 @@
+#!python3
+
+import json
+import time
+
+from kafka import KafkaProducer
+
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+if __name__ == "__main__":
+
+    with open('logFraud.json','rb') as file:
+        file = json.load(file)
+
+    producer = KafkaProducer(bootstrap_servers=['localhost'], 
+                             value_serializer=json_serializer)
+    
+    # push data to kafka topik with name digitalskola
+    while True:
+        for data in file:
+            print(data)
+            producer.send("digitalskola", data)
+            time.sleep(1) # mengambil 1 detik bisa di set, bisa error kalau cepat"
